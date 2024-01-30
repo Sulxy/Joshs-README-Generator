f71e75d810301f73578d95e32d66ed5491589a29
Joshua Hale <joshuahale829@gmail.com>
2024-01-30 09:47:54 -0600

    Updated questions array for the proper format for the license. Wrote writeToFile and init functions.

--- Develop/index.js
+++ Develop/index.js
@@ -17,39 +17,50 @@ const questions = [
         type: 'input',
         name: 'installation',
         message: 'How do you install your project?'
-    }
+    },
     {
         type: 'input',
         name: 'usage',
         message: 'How do you use your project?'
-    }
+    },
     {
         type: 'input',
         name: 'contributing',
         message: 'How can others contribute to your project?'
-    }
+    },
     {
         type: 'input',
         name: 'tests',
         message: 'How do you test your project?'
-    }
+    },
     {
         type: 'input',
         name: 'questions',
         message: 'How can others contact you with questions?(email, github, etc.)'
-    }
+    },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'What license does your project use?'
+        message: 'What license does your project use?',
+        choices: ['MIT', 'Apache 2.0', 'GPLv3', 'BSD 3-Clause', 'None']
+    
     }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log('README.md file has been created!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log(response);
+        writeToFile('README.md', generateMarkdown(response));
+    });
+}
 
 // Function call to initialize app
 init();
